df5e10e7b15a44b85b459a2ae65b3866e618832d
tarun awasthi <tarunawasthi023@gmail.com>
2023-01-31 18:41:00 +0530

    creating hello world rest api

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+	public String helloworld() {
+	
+	return "Hello World";
+	
+	}
+	
+}
