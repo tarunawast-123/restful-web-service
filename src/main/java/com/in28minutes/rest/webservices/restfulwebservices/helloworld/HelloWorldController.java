89853a74ba78b4177dcaea478c503ee02251055c
tarun awasthi <tarunawasthi023@gmail.com>
2023-01-31 19:36:25 +0530

    enhancement in hello world api to understand better about path parameters

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+	@GetMapping(path = "/hello-world")
 	public String helloworld() {
-	
+		
 	return "Hello World";
+	}
 	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloworldBean() {
+		
+	return new HelloWorldBean("Hello World");
+	}
+	
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloworldBeanPathParameter(@PathVariable String name) {
+		
+	return new HelloWorldBean(String.format("Heelo World, %s", name));
 	}
 	
 }
